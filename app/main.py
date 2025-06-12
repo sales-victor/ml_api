60a904796ac9e8e745cba41bf63394d89358bfcc
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2025-06-12 15:44:08 -0300

    Feature: Implementação endpoints XGBoost, Random Forest

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, File, UploadFile
 from etl import run_etl
 from dataframe import get_historical_data
 from models.lstm import predict_lstm
+from models.xgboost import predict_xgboost
+from models.rforest import predict_rforest
 
 app = FastAPI()
 
@@ -14,23 +16,26 @@ async def predict_with_file():
     except Exception as e:
         return {"error": f"Erro ao processar o arquivo CSV: {str(e)}"}
     
+    # LSTM prediction
     try:
         result_lstm = predict_lstm(df)
     except Exception as e:
         return {"error": f"Erro ao executar a predição: {str(e)}"}
     
+    # XGBoost prediction
     try:
-        result_arima = "predict_arima(df)"
+        result_xgb = predict_xgboost(df)
     except Exception as e:
-        return {"error": f"Erro ao executar a predição: {str(e)}"}
-    
+       return {"error": f"Erro ao executar XGBoost: {str(e)}"}
+
+    # Random Forest prediction
     try:
-        result_forecast = "predict_forecast(df)"
+        result_rf = predict_rforest(df)
     except Exception as e:
-        return {"error": f"Erro ao executar a predição: {str(e)}"}
+        return {"error": f"Erro ao executar Random Forest: {str(e)}"}
 
     return {
-        "result_arima": result_arima, 
-        "result_forecast": result_forecast, 
-        "result_lstm": result_lstm
+        "result_lstm": result_lstm,
+        "result_xgboost": result_xgb,
+        "result_rforest": result_rf
         }
